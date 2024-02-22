35630c6004fc7bbf371e1e6309912a0450c6572a
zaid50015 <mohmmadzaid759@gmail.com>
2024-02-22 13:56:41 +0530

    some changes

--- mqtt_json_config_publisher.cpp
+++ mqtt_json_config_publisher.cpp
@@ -14,23 +14,54 @@ using namespace std;
 using namespace rapidjson;
 using namespace libconfig;
 
-// MQTT broker details
-#define MQTT_HOST "localhost"
-#define MQTT_PORT 1883
-#define MQTT_TOPIC "test"
+// // MQTT broker details
+// #define MQTT_HOST "localhost"
+// #define MQTT_PORT 1883
+// #define MQTT_TOPIC "test"
+void changeValuesInJson(Value &value)
+{
+    if (value.IsObject())
+    {
 
-int main() {
+        for (auto &member : value.GetObject())
+        {
+            changeValuesInJson(member.value);
+        }
+    }
+    else if (value.IsArray())
+    {
+
+        for (SizeType i = 0; i < value.Size(); ++i)
+        {
+
+            changeValuesInJson(value[i]);
+        }
+    }
+    else if (value.IsString())
+    {
+
+        value.SetString("hello");
+    }
+    else if (value.IsInt())
+    {
+
+        value.SetInt(0);
+    }
+}
+int main()
+{
     struct mosquitto *mosq = NULL;
     int rc;
 
     mosquitto_lib_init();
     Config cfg;
 
-    try {
+    try
+    {
 
         cfg.readFile("./config/configfile.cfg");
 
-        const Setting& root = cfg.getRoot();
+        const Setting &root = cfg.getRoot();
 
         string host;
         int port;
@@ -41,71 +72,82 @@ int main() {
         root.lookupValue("port", port);
         root.lookupValue("topic", topic);
         root.lookupValue("jsonfile", jsonfile);
- 
 
-    // Create a new mosquitto client instance
-    mosq = mosquitto_new(NULL, true, NULL);
-    if (!mosq) {
-        cerr << "Error: Out of memory when creating client." << endl;
-        return 1;
-    }
-
-    // Connect to MQTT broker
-    //rc = mosquitto_connect(mosq, MQTT_HOST, MQTT_PORT, 60);
-    rc = mosquitto_connect(mosq, host.c_str(), port, 60);
-
-    if (rc) {
-        cerr << "Error: Could not connect to MQTT broker." << endl;
-        return rc;
-    }
-
-    // Loop to publish messages
-    while (true) {
-        // Read JSON object from file
-        //ifstream ifs("data.json");
-        ifstream ifs(jsonfile);
-        if (!ifs.is_open()) {
-            cerr << "Error: Failed to open data.json" << endl;
+        // Create a new mosquitto client instance
+        mosq = mosquitto_new(NULL, true, NULL);
+        if (!mosq)
+        {
+            cerr << "Error: Out of memory when creating client." << endl;
             return 1;
         }
 
-        string content((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
-        ifs.close();
+        // Connect to MQTT broker
+        // rc = mosquitto_connect(mosq, MQTT_HOST, MQTT_PORT, 60);
+        rc = mosquitto_connect(mosq, host.c_str(), port, 60);
 
-        Document document;
-        document.Parse(content.c_str());
-
-        if (document.HasParseError()) {
-            cerr << "Error: Failed to parse JSON." << endl;
-            return 1;
-        }
-
-        StringBuffer buffer;
-        Writer<StringBuffer> writer(buffer);
-        document.Accept(writer);
-        string payload = buffer.GetString();
-
-        // Publish the JSON object as a message
-        rc = mosquitto_publish(mosq, NULL, MQTT_TOPIC, payload.length(), payload.c_str(), 0, false);
-        if (rc) {
-            cerr << "Error: Could not publish message." << endl;
+        if (rc)
+        {
+            cerr << "Error: Could not connect to MQTT broker." << endl;
             return rc;
         }
 
-        cout << "Message published: " << payload << endl;
+        // Loop to publish messages
+        while (true)
+        {
+            // Read JSON object from file
+            // ifstream ifs("data.json");
+            ifstream ifs(jsonfile);
+            if (!ifs.is_open())
+            {
+                cerr << "Error: Failed to open data.json" << endl;
+                return 1;
+            }
 
-        // Sleep for a while before publishing the next message
-        this_thread::sleep_for(chrono::seconds(1));
-      }
-    } catch (const FileIOException &fioex) {
+            string content((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
+            ifs.close();
+
+            Document document;
+            document.Parse(content.c_str());
+            changeValuesInJson(document);
+
+            if (document.HasParseError())
+            {
+                cerr << "Error: Failed to parse JSON." << endl;
+                return 1;
+            }
+
+            StringBuffer buffer;
+            Writer<StringBuffer> writer(buffer);
+            document.Accept(writer);
+          
+            string payload = buffer.GetString();
+
+            // Publish the JSON object as a message
+            rc = mosquitto_publish(mosq, NULL, topic.c_str(), payload.length(), payload.c_str(), 0, false);
+            if (rc)
+            {
+                cerr << "Error: Could not publish message." << endl;
+                return rc;
+            }
+
+            cout << "Message published: " << payload << endl;
+
+            // Sleep for a while before publishing the next message
+            this_thread::sleep_for(chrono::seconds(1));
+        }
+    }
+    catch (const FileIOException &fioex)
+    {
         cerr << "Error reading config file: " << fioex.what() << "Filename :" << endl;
         return 1;
     }
-    catch (const ParseException &pex) {
+    catch (const ParseException &pex)
+    {
         cerr << "Error parsing config file at line " << pex.getLine() << ": " << pex.getError() << endl;
         return 1;
     }
-    catch (const SettingNotFoundException &nfex) {
+    catch (const SettingNotFoundException &nfex)
+    {
         cerr << "Error setting not found in config file: " << nfex.getPath() << endl;
         return 1;
     }
